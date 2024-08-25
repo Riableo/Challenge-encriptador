c9f77e49368643b3090d45195b366ab0a99bc5f8
Leo <rbrian@academia.usbbog.edu.co>
2024-08-25 16:20:10 -0500

    :star: FEATURE: Inclusión deToast.
    Uso de toast para la información al usuario del copiado del texto.

--- app.js
+++ app.js
@@ -74,3 +74,50 @@ function copy() {
         console.log('copiado!'); // * toast with text copied!
     })
 }
+
+// script.js
+
+let icon = {
+    info: '<span class="material-symbols-outlined">info</span>',
+};
+
+
+// js by GeeksforGeeks
+const showToast = (
+    message = "Sample Message",
+    toastType = "info",
+    duration = 5000) => {
+    if (
+        !Object.keys(icon).includes(toastType))
+        toastType = "info";
+
+    let box = document.createElement("div");
+    box.classList.add(
+        "toast", `toast-${toastType}`);
+    box.innerHTML = ` <div class="toast-content-wrapper">
+                      <div class="toast-icon">
+                      ${icon[toastType]}
+                      </div>
+                      <div class="toast-message">${message}</div>
+                      <div class="toast-progress"></div>
+                      </div>`;
+    duration = duration || 5000;
+    box.querySelector(".toast-progress").style.animationDuration =
+            `${duration / 1000}s`;
+
+    let toastAlready = document.body.querySelector(".toast");
+    if (toastAlready) {
+        toastAlready.remove();
+    }
+
+    document.body.appendChild(box)};
+
+let information = document.querySelector(".btn-copy");
+
+
+information.addEventListener("click",(e) => {
+    e.preventDefault();
+    showToast("Texto copiado","info",5000);
+});
+
+
